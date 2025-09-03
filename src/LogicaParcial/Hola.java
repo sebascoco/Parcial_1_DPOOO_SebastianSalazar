eddb21698c73a0b550831edaff69526b66a54857
sebascoco <s.salazars2@uniandes.edu.co>
2025-09-02 19:46:50 -0500

    primer commit

---
+++ src/LogicaParcial/Hola.java
@@ -0,0 +1,11 @@
+package LogicaParcial;
+
+public class Hola {
+		public static void main(String[] args) {
+		System.out.println("Hola, mundo!");
+	}
+		static void metodo() {
+			System.out.println("Hola, mundo!");
+		}
+}
+// Compare this snippet from Banco.java:
